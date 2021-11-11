5a9cef2ea83b809633a488e1281800096a3e5869
giseledoan <88763674+giseledoan@users.noreply.github.com>
2021-11-11 10:04:12 -0800

    Edited logic.js, added cities.js

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -1,13 +1,24 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+
 //  Add a marker to the map for Los Angeles, California.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: '#ffffa1'
- }).addTo(map);
+let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+
+// Get data from cities.js
+let cityData = cities;
+ 
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius:city.population/200000,
+        color: "orange"
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
