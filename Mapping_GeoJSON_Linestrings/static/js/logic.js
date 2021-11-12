f54d14a731bd4f36a0116b8cf9ca978635b6b0e2
giseledoan <88763674+giseledoan@users.noreply.github.com>
2021-11-12 12:41:51 -0800

    editted logic.js to have yellow line

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -17,7 +17,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: light,
+  Light: light,
   Dark: dark
 };
 
@@ -25,7 +25,7 @@ let baseMaps = {
 let map = L.map('mapid', {
   center: [44, -80],
   zoom: 2,
-  layers: [streets]
+  layers: [light]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -35,14 +35,22 @@ L.control.layers(baseMaps).addTo(map);
 light.addTo(map);
 
 // Add GeoJSON data.
-let torontoData = "https://raw.githubusercontent.com/giseledoan/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
+let torontoData = "https://raw.githubusercontent.com/giseledoan/Mapping_Earthquakes/main/torontoRoutes.json";
 
-// Grabbing our GeoJSON data.
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
+// Grabbing our GeoJSON data
 d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data).addTo(map);
+L.geoJson(data, {
+style: myStyle,
+onEachFeature: function(feature, layer){
+  layer.bindPopup("<h3> Airline:" + feature.properties.airline + "</h3><hr><h3> Destination:"
+   + feature.properties.dst+ "</h3>");
+}
+}).addTo(map);
 });
-
-
-
